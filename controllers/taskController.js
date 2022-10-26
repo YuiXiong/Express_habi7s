334cb8d6f01f12a27d540fc51c1a3ab273e3ef34
Yui Xiong Ng <jadebear15@gmail.com>
2022-10-27 00:12:20 +0800

    feat:index array of task objectID

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -14,19 +14,38 @@ module.exports = {
         quadrant: req.body.quadrant,
         board: req.params.id,
       });
-      console.log("task creation done", taskCreate._id)
+      console.log("task creation done", taskCreate._id);
 
-      await boardModel.findByIdAndUpdate(req.params.id,{
-        $push:{
-          tasks: taskCreate._id
-        }
-      })
-      console.log("done")
+      await boardModel.findByIdAndUpdate(req.params.id, {
+        $push: {
+          tasks: taskCreate._id,
+        },
+      });
+      console.log("done");
     } catch (err) {
       console.log(err);
       return res.status(500).json({ error: "failed to create task" });
     }
 
     return res.json();
+  },
+
+  index: async (req, res) => {
+    //index task given board object id. board task is an array of objectID. to populate and return task.
+
+    try {
+      boards = await boardModel.findById(req.params.id);
+      
+      console.log("boards detail: ", boards);
+      let mappingTask=[]
+      mappingTask = boards.tasks.map(task =>{
+        return task
+      })
+      console.log("mappingTask: ", mappingTask)
+    } catch {
+      res.send(500);
+      return res.json({ error: "failed to return board" });
+    }
+    return res.json(boards);
   },
 };
--- routers/taskRoutes.js
+++ routers/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const taskController = require('../controllers/taskController')
 
 router.post("/:id/task/", taskController.create);
-// router.get("/", taskController.index)
+router.get("/:id/task/", taskController.index)
 // router.get("/:id", taskController.getById)
 // router.patch("/:id", taskController.updateById)
 // router.delete("/:id", taskController.deleteById)
